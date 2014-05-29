7b955f8bb53c4aea696f6ee5de007bbd61b8b4af
chris <chris.j.major+git@gmail.com>
2014-05-29 18:31:15 +0100

    Clearing up worksheets

--- .worksheet/src/othello.info.Evolution.scala
+++
@@ -1,5 +0,0 @@
-package othello.info
-
-object Evolution {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(84); 
-  println("Welcome to the Scala worksheet")}
-}
